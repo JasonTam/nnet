b672d018bea318a3ea38e6d53ef4a69a0a084e65
JasonTam <JasonTam22@gmail.com>
2013-11-14 21:48:09 -0500

    parseFeatures handle more than 1 output

--- parseFeatures.m
+++ parseFeatures.m
@@ -11,11 +11,11 @@ end
 fID = fopen(fPath);
 % [N_e N_i N_o]
 n = cell2mat(textscan(fID,'%d %d %d',1));
-fspec = repmat('%f ',[1 n(2)+1]);
+fspec = repmat('%f ',[1 n(2)+n(3)]);
 A = cell2mat(textscan(fID,fspec,n(1)));
 fclose(fID);
 
-F = A(:,1:end-1); T = A(:,end);
+F = A(:,1:end-n(3)); T = A(:,end-n(3)+1:end);
 train.n=n; train.features=F; train.targets=T;
 
 end
