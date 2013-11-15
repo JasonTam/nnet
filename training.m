40e1ada15406531ece27c7d1834917e40b63e4d6
JasonTam <JasonTam22@gmail.com>
2013-11-14 21:42:58 -0500

    changed function call name

--- training.m
+++ training.m
@@ -1,15 +1,19 @@
 % Jason Tam
 
-initPath = './data/WDBC/sample.NNWDBC.init';
-trainPath = './data/WDBC/wdbc.train';
+% initPath = './data/WDBC/sample.NNWDBC.init';
+% trainPath = './data/WDBC/wdbc.train';
+initPath = './data/grades/sample.NNGrades.init';
+trainPath = './data/grades/grades.train';
+
+
 
 init = parseNN(initPath);
-train = parseTrain(trainPath);
+train = parseFeatures(trainPath);
 
 n_epochs = 100;
-alpha = 0.1;
+alpha = 0.05;
 NN = backPropLearn(init,train,n_epochs,alpha);
 
-writeNNtoFile('./lol.txt',NN);
+writeNNtoFile('./grades.txt',NN);
 
 
