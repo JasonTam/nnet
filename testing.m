9bde5ff6289ddebd83ca042db7491df5aee89617
jason <JasonTam22@gmail.com>
2013-11-30 20:13:02 -0500

    external dataset support

--- testing.m
+++ testing.m
@@ -3,9 +3,14 @@
 
 % testPath = './data/WDBC/wdbc.test';
 
-nnPath = './myOut/trainedNN_grades.txt';
-testPath = './data/grades/grades.test';
-outPath = './myOut/results_grades.txt';
+% nnPath = './myOut/trainedNN_grades.txt';
+% testPath = './data/grades/grades.test';
+% outPath = './myOut/results_grades.txt';
+
+nnPath = './myOut/trainedNN_prostate.txt';
+testPath = './myData/prostate/prostate.test';
+outPath = './myOut/results_prostate.txt';
+
 
 testNN( nnPath, testPath, outPath )
 
--- training.m
+++ training.m
@@ -6,11 +6,14 @@
 % outPath = './myOut/trainedNN_WDBC.txt';
 % n_epochs = 100; alpha = 0.1;
 
-initPath = './data/grades/sample.NNGrades.init';
-trainPath = './data/grades/grades.train';
-outPath = './myOut/trainedNN_grades.txt';
-n_epochs = 100; alpha = 0.05;
+% initPath = './data/grades/sample.NNGrades.init';
+% trainPath = './data/grades/grades.train';
+% outPath = './myOut/trainedNN_grades.txt';
+% n_epochs = 100; alpha = 0.05;
 
-
+initPath = './myData/prostate/sample.NNProstate.init';
+trainPath = './myData/prostate/prostate.train';
+outPath = './myOut/trainedNN_prostate.txt';
+n_epochs = 200; alpha = 0.05;
 
 trainNN( n_epochs, alpha, initPath, trainPath, outPath );
